3dddaffe76ed7eabeb8d33fef1935788f7923fe9
ChoiYoonSoo <gews30025@naver.com>
2024-12-10 16:35:33 +0900

    기본 UI 컴포저블 개념을 적용하여 특정 문제에 관한 화면 구현

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.testproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,9 +21,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -89,13 +94,135 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun LearnTogether(title: String, subTitle: String, text: String){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = subTitle,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = text,
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun TaskManager(title: String, subTitle: String){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = subTitle,
+            fontSize = 16.sp
+        )
+
+    }
+}
+
+@Composable
+fun Quarters(){
+    Column(
+       Modifier.fillMaxWidth()
+    ) {
+        Row(Modifier.weight(1f)) {
+            QuartersText(
+                title = stringResource(R.string.Quarters_first_title),
+                text = stringResource(R.string.Quarters_first_text),
+                backColor = colorResource(R.color.Quarters_first_color),
+                modifier = Modifier.weight(1f)
+            )
+            QuartersText(
+                title = stringResource(R.string.Quarters_second_title),
+                text = stringResource(R.string.Quarters_second_text),
+                backColor = colorResource(R.color.Quarters_second_color),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            QuartersText(
+                title = stringResource(R.string.Quarters_third_title),
+                text = stringResource(R.string.Quarters_third_text),
+                backColor = colorResource(R.color.Quarters_thirds_color),
+                modifier = Modifier.weight(1f)
+            )
+            QuartersText(
+                title = stringResource(R.string.Quarters_fourth_title),
+                text = stringResource(R.string.Quarters_fourth_text),
+                backColor = colorResource(R.color.Quarters_fourth_color),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun QuartersText(title: String, text: String, backColor: Color, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier // modifier를 전달 받음으로써 균등한 범위 내에서만 보이도록
+            .fillMaxSize()
+            .background(backColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+        )
+        Text(
+            text = text,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     TestProjectTheme {
-        GreetingImage(
-            message = stringResource(R.string.happy_birthday_text),
-            from = stringResource(R.string.signature_text)
-        )
+//        LearnTogether(
+//            title = stringResource(R.string.LearnTogether_title),
+//            subTitle = stringResource(R.string.LearnTogether_subTitle),
+//            text = stringResource(R.string.LearnTogether_text)
+//        )
+//        TaskManager(
+//            title = stringResource(R.string.TaskManager_title),
+//            subTitle = stringResource(R.string.TaskManager_subTitle)
+//        )
+        Quarters()
     }
 }
